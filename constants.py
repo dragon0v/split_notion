cb901c08960f18aecafa71236bd9a50d9c35406e
dragon0v <dragon0v@protonmail.com>
2025-04-19 11:18:19 +0200

    update

--- constants.py
+++ constants.py
@@ -1,9 +1,10 @@
 # exchange rate from currency 0 to currency 1 
 EXCHANGE_RATE_LOCAL = {
+    "description": "4-12到4-18的xe平均汇率",
     "SEK": {
-        "NOK": 1.09,
+        "NOK": 1 / 0.922346,
     },
     "NOK": {
-        "SEK": 0.92,
+        "SEK": 0.922346,
     },
 }
